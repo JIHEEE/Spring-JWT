658fca4d43796a92f1def96bb4fccebbb2aa18b8
CJH <choij928@gmail.com>
2022-01-26 14:26:12 +0900

    jwt 수정

--- Spring-JWT/src/main/java/com/oopsw/jh/web/TestJWT.java
+++ Spring-JWT/src/main/java/com/oopsw/jh/web/TestJWT.java
@@ -26,15 +26,40 @@ public class TestJWT {
 		
 	}
 	
+	
+	/**
+	 * Header
+	 * · alg : signature를 해싱하기 위한 알고리즘 정보를 갖고 있음
+	 * · typ : 토큰의 타입을 나타냄, 생략 가능, 보통 JWT 사용
+	 * 
+	 * 
+	 * Payload
+	 * - server와 client가 주고받는 시스템에서 실제로 사용 될 정보에 대한 내용을 담고있음
+	 * - JWT가 기본적으로 갖고있는 키워드가 존재, 추가 가능
+	 * · iss : 토큰 발급자
+	 * · sub : 토큰 제목
+	 * · aud : 토큰 대상
+	 * · exp : 토큰 만료 시간
+	 * · nbf : Not Before
+	 * · iat : 토큰이 발급된 시간
+	 * · jti : JWT의 고유 식별자
+	 * 
+	 * Signature
+	 * - 서버에서 토큰이 유효한지 검증하기 위한 문자열 
+	 * - Header + Payload + Secret Key로 값을 생성하므로 데이터 변조 여부를 판단 가능
+	 * - Secret Key는 노출되지 않도록 서버에서 관리 필요
+	 * */
+	
+	
 	// token 생성
 	public String createToken() {
 		
 		// Header 부분 설정
 		Map<String, Object> headers = new HashMap<>();
-		headers.put("typ", "JWT");
-		headers.put("alg", "HS256");
+		headers.put("typ", "JWT");		
+		headers.put("alg", "HS256");	
 		
-		// payload 부분 설정
+		// payload(내용) 부분 설정
 		Map<String, Object> payloads = new HashMap<>();
 		payloads.put("data", "My First JWT!");
 		
@@ -45,10 +70,10 @@ public class TestJWT {
 		
 		// token builder
 		String jwt = Jwts.builder()
-                .setHeader(headers) // Headers 설정
+                .setHeader(headers)  // Headers 설정
                 .setClaims(payloads) // Claims 설정
-                .setSubject("user") // 토큰 용도 
-                .setExpiration(ext) // 토큰 만료 시간 설정
+                .setSubject("user")  // 토큰 용도 
+                .setExpiration(ext)  // 토큰 만료 시간 설정
                 .signWith(SignatureAlgorithm.HS256, key.getBytes()) // HS256과 Key로 Sign
                 .compact(); // 토큰 생성
 		
